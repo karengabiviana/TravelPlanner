3c2df9d55af6c0d7f8056bee87bf36e3ad3129ca
Karen Gabrieli <karen.oliveira@sumup.com>
2023-11-08 11:09:09 -0300

    First Commit

---
+++ TravelPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelPlanner
+//
+//  Created by Karen Oliveira on 08/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
